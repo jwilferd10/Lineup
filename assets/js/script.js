ffe1b9256db880c76383001f45910f14cd6efa15
Jonathan Wilferd <jwilferd10@yahoo.com>
2023-01-21 23:35:32 -0800

    Feat: Psuedocode added

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+// create empty array for localStorage
+
+// .val of textField dynamically generated in htmlEl
+
+// create a button that deletes the generated content.
+
+///////////////////////////////////
+// Function - deleteEntry 
+
+// When deleteBtn is clicked, delete single task 
